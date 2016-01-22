1945c149936fc311d91f6fd01894b95438e0dedd
wenfeng.zhong@gmail.com <wenfeng.zhong@gmail.com>
2016-01-22 16:52:02 +0800

    update

--- test.js
+++ test.js
@@ -4,12 +4,23 @@ var fs = require('fs');
 
 var screenNum = 0;
 var screenName;
-var sleeptime = 2000;
-var loginSleepTime = 1000;
+var sleeptime = 100;
+var loginSleepTime = 100;
 //var sleeptime = 1;
 var request = require('request');
 var displayText = [];
 displayTextCount = 0;
+var data_url = 'http://node.jprda.com';
+var currentXnxq = "2015-2016-1";
+var currentCxkg = "X";
+var enableBrowser = true;
+//var enableBrowser = false;
+var driver;
+var s_testCases;
+var s_grades;
+var url = "http://test.zhjw.syx.thcic.cn:21122/";
+var saveFilePath = "C:\\Users\\zhong\\Desktop\\screen\\"; 
+
 var testUsers = [
 /*
 {xh : "2009980013",xslx : "yx",sfxs : "yes"}//院系
@@ -40,15 +51,31 @@ var testUsers = [
 	xslx : "yjs",
 	sfxs : "是"
 }, 
+{
+	xh : "2015211059",
+	xslx : "ss",
+	sfxs : "是"
+},
  {
 	xh : "2015310556",
 	xslx : "yjs",
 	sfxs : "是"
 }
-
+,
+{
+	xh : "2015310556",
+	xslx : "bs",
+	sfxs : "是"
+}
 , {
 	xh : "2014212099",
 	xslx : "yjs",
+	sfxs : "否"
+}
+
+, {
+	xh : "2014212099",
+	xslx : "ss",
 	sfxs : "否"
 }
 , {
@@ -56,42 +83,61 @@ var testUsers = [
 	xslx : "yjs",
 	sfxs : "否"
 }
+, {
+	xh : "2014310401",
+	xslx : "bs",
+	sfxs : "否"
+}
 ,{xh : "2015400536",xslx : "jxs",sfxs : "是"} //进修生
 
 
  ];
 
-var data_url = 'http://node.jprda.com';
-var currentXnxq = "2015-2016-1";
-var currentCxkg = "关";
-var driver;
-var s_testCases;
-var s_grades;
-var url = "http://test.zhjw.syx.thcic.cn:21122/";
-var saveFilePath = "C:\\Users\\zhong\\Desktop\\screen\\";
 
-request.get({
-	url : data_url + "/grade/",
-	json : true,
-	"encoding" : "utf-8"
-}, function(e, r, gs) {
-	// console.log(students);
-	s_grades = gs;
-
+	
 	request.get({
-		url : data_url + "/testCase/?sort=xh%20ASC",
-		json : true,
-		"encoding" : "utf-8"
-	}, function(e, r, testCases) {
-		// console.log(testCases);
-		s_testCases = testCases;
-		start();
-	});
+		url : "http://166.111.5.112:21122/tools/oracle.jsp",
+		json : true
+	}, function(e, r, gs) {
+		console.log(gs);
+		if(gs.result == 'on'){ 
+			currentCxkg = "开";
+		//	currentCxkg = "ON";
+		}else if(gs.result == 'off'){
+			currentCxkg = "关";
+		//	currentCxkg = "OFF"; 
+		}
+		console.log("currentCxkg:" +currentCxkg);
 
-});
+		request.get({
+			url : data_url + "/grade/",
+			json : true,
+			"encoding" : "utf-8"
+		}, function(e, r, gs) {
+			s_grades = gs;
+			request.get({
+				url : data_url + "/testCase/?sort=xh%20ASC",
+				json : true,
+				"encoding" : "utf-8"
+			}, function(e, r, testCases) {
+				s_testCases = testCases;
+				start();
+			});
+
+		});
+		
+	});
+	
+	
+
+
+
+
+
+
+
 
 function loign(xh) {
-
 	if (enableBrowser) {
 		driver.get(url + "login.do");
 		console.log('login.do>>>' + xh);
@@ -101,24 +147,14 @@ function loign(xh) {
 		driver.findElement(By.name('j_password')).sendKeys('123');
 		driver.findElement(By.name('bt')).click();
 		driver.sleep(loginSleepTime);
-		// driver.wait(until.titleIs('综合教务'), 1000);
 	}
-
 }
 
-var enableBrowser = true;
-// var enableBrowser = false;
-
-function clickLink(link) {
-	// link.click();
-	console.log('clickLink>>>' + link);
-}
 
 function start() {
 	if (enableBrowser) {
 		driver = new webdriver.Builder().forBrowser('chrome').build();
 		//driver = new webdriver.Builder().forBrowser(webdriver.Browser.INTERNET_EXPLORER).build();
-		
 		driver.get(url + "xklogin.do");
 		driver.sleep(loginSleepTime);
 	}
@@ -127,7 +163,6 @@ function start() {
 	 if(folder_exists == true)
 	 {
 	      var dirList = fs.readdirSync(saveFilePath);
-	  
 	     dirList.forEach(function(fileName)
 	      {
 	    	 console.log("del......" + saveFilePath + fileName);
@@ -152,13 +187,16 @@ function start() {
 			if (enableBrowser) {
 				driver.get(testCase.url);
 				screenNumName = testCase.xh;
-
-				// driver.findElement(By.xpath("//body")).sendKeys(webdriver.Key.KEY_PAGE_DOWN);
 				driver.sleep(sleeptime);
+				var studentType = "";
+				if("是" == testUser.sfxs){
+					studentType = "" + currentCxkg + "-(新生)";
+				}else if("否" == testUser.sfxs){
+					studentType = "" + currentCxkg + "-(老生)";
+				}
 				
 				
-				displayText[displayText.length] = testCase.title;
-				
+				displayText[displayText.length] = studentType + testUser.xh + "-"+ testCase.title;
 				driver.takeScreenshot(1).then(
 						function(data) {
 							
@@ -168,7 +206,7 @@ function start() {
 									var txt = "";
 							txt +=displayText[displayTextCount];
 							fs.writeFile(saveFilePath  
-									+ screenNum + "-"+ txt.replace('\t','') +".png", base64Data,
+									+ txt.replace('\t','') +".png", base64Data,
 									'base64', function(err) {
 										if (err)
 											console.log(err);
